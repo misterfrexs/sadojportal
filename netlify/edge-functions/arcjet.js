510e6181a744d109b942ef8faf9ee83d3d911490
Andrew Clemenceau <clemenceauandrew@gmail.com>
2026-04-14 21:49:14 +0200

    Correction : Utilisation du module Deno pour Arcjet

--- netlify/edge-functions/arcjet.js
+++ netlify/edge-functions/arcjet.js
@@ -1,7 +1,7 @@
-import arcjet, { detectBot, shield } from "https://esm.sh/@arcjet/netlify";
+import arcjet, { detectBot, shield } from "https://esm.sh/@arcjet/deno";
 
 const aj = arcjet({
-  key: Netlify.env.get("ajkey_01knf9hs69f7z8ndf33ss5ct3t"), // Récupère la clé Arcjet depuis les variables d'environnement de Netlify
+  key: Netlify.env.get("ajkey_01knf9hs69f7z8ndf33ss5ct3t"),
   rules: [
     shield({ mode: "LIVE" }),
     detectBot({ mode: "LIVE", allow: [] }),
@@ -12,10 +12,10 @@ export default async (request, context) => {
   const decision = await aj.protect(request);
 
   if (decision.isDenied()) {
-    return new Response("Accès refusé par Arcjet", { status: 403 });
+    return new Response("Accès refusé par Arcjet pour SADOJ", { status: 403 });
   }
 
   return context.next();
 };
 
-export const config = { path: "/*" }; // Protège TOUTES les pages du site
+export const config = { path: "/*" };
